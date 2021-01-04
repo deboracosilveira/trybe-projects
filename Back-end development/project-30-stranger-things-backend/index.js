d2c45425148744366bbfdfacb7592a19008dbdd9
deboracosilveira <deboracosilveira@gmail.com>
2021-01-04 12:28:08 -0300

    Project 30

---
+++ Back-end development/project-30-stranger-things-backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+const PORT = process.env.PORT;
+
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(req.query, hereIsTheUpsideDown);
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta${PORT}`);
+});
